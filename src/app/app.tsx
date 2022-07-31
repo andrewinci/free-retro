9ecbdd4dc7a0bedcf6d9930501616f850f3c560e
andrewinci <andrewinci@gmail.com>
2022-07-31 17:02:26 +0100

    Add dummy react app

---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <div>
+    <h1>Free retro</h1>
+  </div>
+);
+
+ReactDOM.render(<App></App>, document.getElementById("root"));
