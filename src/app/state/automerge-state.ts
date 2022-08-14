e165223d85dea1571c2164e53be5c38fd2a25d7f
andrewinci <andrewinci@gmail.com>
2022-08-14 18:50:17 +0200

    do not persist the state

--- src/app/state/automerge-state.ts
+++ src/app/state/automerge-state.ts
@@ -1,10 +1,8 @@
 import * as Automerge from "automerge";
-import { toBase64, toBinaryDocument } from "../helper/binary-document";
+import { toBinaryDocument } from "../helper/binary-document";
 import { randomId } from "../helper/random";
 import { broadcast } from "../ws";
 import { AppState, Stage } from "./model";
-
-const LOCAL_STATE_KEY = "free-retro:state";
 
 const observable = new Automerge.Observable();
 let appState: AppState;
@@ -35,7 +33,6 @@ export async function changeState(f: (state: AppState) => void) {
   appState = Automerge.change(appState, (s) => f(s));
   // broadcast to all clients
   await broadcast(appState.sessionId, appState);
-  localStorage.setItem(LOCAL_STATE_KEY, toBase64(Automerge.save(appState)));
 }
 
 export function loadNewState(remoteRawState: string) {
@@ -43,5 +40,4 @@ export function loadNewState(remoteRawState: string) {
     toBinaryDocument(remoteRawState)
   );
   appState = Automerge.merge(appState, remoteState);
-  localStorage.setItem(LOCAL_STATE_KEY, remoteRawState);
 }
