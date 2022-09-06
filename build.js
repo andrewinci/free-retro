6423e7ace2134d23a4a91b6a69a26c4e0ffe240d
Andrea <andrewinci@gmail.com>
2022-09-06 04:51:09 +0100

    feat: use vite to deal with frontend HMR and build (#70)
    * add mantine
    
    * build: use vite to deal with frontend HMR and build
    
    * pin node/yarn
    
    * add missing react plugin in build
    
    * remove autoprefixer
    
    * remove mantine

---
+++ build.js
@@ -0,0 +1,81 @@
+import esbuild from "esbuild";
+import process, { exit } from "process";
+
+import { build, createServer } from "vite";
+import react from "@vitejs/plugin-react";
+
+const esbuildRun = (options) =>
+  esbuild
+    .build({
+      ...options,
+      logLevel: "info",
+    })
+    .catch((err) => {
+      process.stderr.write(err.stderr);
+      process.exit(1);
+    });
+
+const buildLambdaOptions = (entryPoint, outdir) => ({
+  entryPoints: [entryPoint],
+  bundle: true,
+  outdir,
+  platform: "node",
+  sourcemap: true,
+  minify: process.env.NODE_ENV === "production",
+});
+
+if (process.argv.length < 3) {
+  console.error("Missing build parameter.");
+  console.error("Provide one of: `app`, `lambda` or `all`");
+  exit(1);
+}
+
+const buildApp = async () =>
+  await build({
+    build: {
+      outDir: "./dist/app",
+    },
+    esbuild: {
+      jsxInject: `import * as React from 'react'`,
+    },
+  });
+
+const main = async () => {
+  const parameter = process.argv[2];
+
+  switch (parameter) {
+    case "app":
+      await buildApp();
+      break;
+    case "lambda":
+      esbuildRun(buildLambdaOptions("src/lambda/handler.ts", "dist/lambda"));
+      esbuildRun(
+        buildLambdaOptions("src/tg-forwarder/handler.ts", "dist/tg-forwarder")
+      );
+      break;
+    case "all":
+      await buildApp();
+      esbuildRun(buildLambdaOptions("src/lambda/handler.ts", "dist/lambda"));
+      esbuildRun(
+        buildLambdaOptions("src/tg-forwarder/handler.ts", "dist/tg-forwarder")
+      );
+      break;
+    case "serve":
+      {
+        const server = await createServer({
+          plugins: [react()],
+          // eslint-disable-next-line no-undef
+          root: __dirname,
+        });
+        await server.listen();
+        server.printUrls();
+      }
+      break;
+    default:
+      console.error("Invalid build parameter provided.");
+      console.error("Provide one of: `app`, `lambda` or `all`");
+      exit(1);
+  }
+};
+
+main();
