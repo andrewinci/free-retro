e85d9b7e56b984d51883e959413629f02fb8c272
Sam Cooper <003.sjc@gmail.com>
2023-06-03 11:10:42 +0100

    feat: Option to include a kudos column (#243)

--- src/app/components/column.tsx
+++ src/app/components/column.tsx
@@ -4,7 +4,7 @@ import { useDrop } from "react-dnd";
 import styled from "@emotion/styled";
 import { Id } from "../state";
 import { TextArea } from "./textarea";
-import { IconPlus } from "@tabler/icons";
+import { IconPlus } from "@tabler/icons-react";
 
 type ColumnProps = {
   title: string;
